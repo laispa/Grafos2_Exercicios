30066ee9264e2316493456d42515d0d079b277a8
Lais Portela <lais3722@gmail.com>
2024-07-13 21:37:12 -0300

    resolução do 1 exercicio

--- Exercicios/dijkstra.cpp
+++ Exercicios/dijkstra.cpp
@@ -1,18 +1,20 @@
 
-
 #include <bits/stdc++.h>
+#include <climits>
 
 using namespace std;
-using ii = pair<int, int>;
+using ii = pair<long long, long long>;
 
-const int MAX { 100010 };
+const long long  MAX { 100010 };
 vector<ii> adj[MAX];
+vector<long long> path;
 
-vector<int> dijkstra(int s, int N, vector<int>& parent)
+
+vector<long long> dijkstra(long long s, long long  N, vector<long long>& parent)
 {
-    const int oo { 1000000010 };
+    const long long  oo { LONG_LONG_MAX };
 
-    vector<int> dist(N + 1, oo);
+    vector<long long > dist(N + 1, oo);
     dist[s] = 0;
     parent[s] = -1;
 
@@ -41,9 +43,42 @@ vector<int> dijkstra(int s, int N, vector<int>& parent)
     return dist;
 }
 
+void findPath( long long n, vector<long long>& parent) {
+    std::vector<long long> temp;
 
+    while (n != -1) {
+        temp.push_back(n); 
+        n = parent[n];
+    }
+
+    for (int i = temp.size() - 1; i >= 0; --i) {
+        path.push_back(temp[i]);
+    }
+
+}
 
 int main(){
+    long long  n, m;
+    cin >> n >> m;
+
+    for (long long i = 0; i < m; i++) {
+        long long a, b, w;
+        cin >> a >> b >> w;
+        adj[a].push_back(ii(b, w));
+        adj[b].push_back(ii(a, w));
+    }
+
+    vector<long long> parent(n + 1, -1);
+    auto dist = dijkstra(1, n, parent);
+
+    if (dist[n] == LONG_LONG_MAX) {
+        cout << -1 << '\n';
+    } else {
+        findPath(n, parent);
+        for (long long  result : path)
+            cout << result << ' ';
+        cout << '\n';
+    }
 
     return 0;
 }
